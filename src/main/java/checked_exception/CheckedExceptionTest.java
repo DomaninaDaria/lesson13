05c3fd8623097068e9f062023c427753c99c5903
[Daria] <33488966+DomaninaDaria@users.noreply.github.com>
2019-07-18 16:08:22 +0300

    lalala

--- src/main/java/checked_exception/CheckedExceptionTest.java
+++ src/main/java/checked_exception/CheckedExceptionTest.java
@@ -7,6 +7,7 @@ public class CheckedExceptionTest {
     public static void main(String[] args) throws FileNotFoundException {
         VeryImportantSystem veryImportantSystem = new VeryImportantSystem();
         veryImportantSystem.someMethodWithCheckedException();
+        System.out.println("dsfdsf");
     }
 
 }
